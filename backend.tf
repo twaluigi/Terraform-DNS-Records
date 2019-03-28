ab6bd0ae2fcf6efa0fca3d9a8110dda850a02a2d
EC2 Default User <thomas.wygonik@gmail.com>
2019-03-28 21:19:46 +0000

    backend changes, moving website records to other terraform project

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tomwygonik-tfstate"
-    key    = "terraform/dns/terraform_dns_project.tfstate"
-    region = "us-east-2"
+    bucket         = "tomwygonik-tfstate"
+    key            = "terraform/dns/terraform_dns_project.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "tomwygonik-tfstate-lock"
+    encrypt        = true
   }
 }
--- common.tf
+++ common.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = "${var.aws_region}"
+  region  = "${var.aws_region}"
+  version = "~>2.0"
 
   #profile = "${var.aws_profile}"
 }
