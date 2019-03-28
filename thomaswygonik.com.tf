7147b2a0f50577d10fcbfa17ec3415a5e6e6b012
Thomas Wygonik <thomas.wygonik@gmail.com>
2019-03-28 17:24:36 -0400

    Adding TXT record for gsuite

--- thomaswygonik.com.tf
+++ thomaswygonik.com.tf
@@ -2,3 +2,11 @@ resource "aws_route53_zone" "thomaswygonik_com" {
   name              = "thomaswygonik.com"
   delegation_set_id = "${aws_route53_delegation_set.main.id}"
 }
+
+resource "aws_route53_zone" "gsuite_txt_thomaswygonik_com" {
+  zone = "${aws_route53_zone.thomaswygonik_com.id}"
+  name = "thomaswygonik.com"
+  type = "TXT"
+  ttl = "300"
+  records = ["google-site-verification=FIeKiNnawjhTxNBa8jfh5cx9heNE92e_8UePxscpwts"]
+}
