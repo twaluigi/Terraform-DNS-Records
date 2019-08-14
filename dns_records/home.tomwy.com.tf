dc4ce299f3ca58ec7ee102ad234f18683b54e5d3
Tom W <thomas.wygonik@gmail.com>
2019-08-13 21:50:35 -0700

    Fix zone for home.tomwy.com records

--- dns_records/home.tomwy.com.tf
+++ dns_records/home.tomwy.com.tf
@@ -4,7 +4,7 @@ resource "aws_route53_zone" "home_tomwy_com"{
 
 # SPF Record for Mailgun
 resource "aws_route53_record" "spf_home_tomwy_com" {
-  zone_id = "${aws_route53_zone.tomwy_com.zone_id}"
+  zone_id = "${aws_route53_zone.home_tomwy_com.zone_id}"
   name    = "home.tomwy.com"
   type    = "TXT"
   records = ["v=spf1 include:mailgun.org ~all"]
@@ -13,7 +13,7 @@ resource "aws_route53_record" "spf_home_tomwy_com" {
 
 # DKIM record for Mailgun
 resource "aws_route53_record" "dkim_home_tomwy_com" {
-  zone_id = "${aws_route53_zone.tomwy_com.zone_id}"
+  zone_id = "${aws_route53_zone.home_tomwy_com.zone_id}"
   name    = "smtp._domainkey.home.tomwy.com"
   type    = "TXT"
   records = ["k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqrVq3Vn/2h4YSZpgqnZLnV4UhTOLVQJBGrHxUix9i57ZyHG7EC1COYz6T79iQ8TDuzFIsihaMyxuLOS/hgspcXfLvpCicFgt7sW474fcyCIOwSbeQNYgzY1kiU0cDKiqNaeXzzsqT0QFuBxffy8WD8KFl2thLxli3MlBjRK3NU8MSIAlw2vMg6DMbJh9mtThY/gflQPyl\"\"gYLzz+9P7iAsv7249cyKi92iG2VQTdZTZ1pjTBQP5//nAFIJu21Os+QFLK13iF7alpRbSRSn0IjmX5COqMnXD4Bxxnz86nYpDG20cx7QRpK4R4KARx5e+q5oWDp1uZD8F+f6kbxM01l8wIDAQAB"]
@@ -22,7 +22,7 @@ resource "aws_route53_record" "dkim_home_tomwy_com" {
 
 # MX Records for Mailgun
 resource "aws_route53_record" "mxa_home_tomwy_com" {
-  zone_id = "${aws_route53_zone.tomwy_com.zone_id}"
+  zone_id = "${aws_route53_zone.home_tomwy_com.zone_id}"
   name    = "home.tomwy.com"
   type    = "MX"
   records = ["10 mxa.mailgun.org", "10 mxb.mailgun.org"]
@@ -31,7 +31,7 @@ resource "aws_route53_record" "mxa_home_tomwy_com" {
 
 # CNAME for tracking opens, clicks, unsubscribes
 resource "aws_route53_record" "mgtracking_home_tomwy_com" {
-  zone_id = "${aws_route53_zone.tomwy_com.zone_id}"
+  zone_id = "${aws_route53_zone.home_tomwy_com.zone_id}"
   name    = "email.home.tomwy.com"
   type    = "CNAME"
   records = ["mailgun.org"]
