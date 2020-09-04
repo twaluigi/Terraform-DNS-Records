e7607e94918d969fe9b26cbf72990e5452ca330f
Tom Wygonik <thomas.wygonik@gmail.com>
2020-09-04 14:33:22 -0700

    Fix syntax for master account number

--- dns_records/main.tf
+++ dns_records/main.tf
@@ -1,8 +1,8 @@
 # Master account for Route 53 records and state store
 provider "aws" {
-  version             = "~>2.0"
+  version             = "~>3.5.0"
   region              = var.region
-  allowed_account_ids = var.master_account_number
+  allowed_account_ids = var.master_account_numbers
 }
 
 terraform {
--- dns_records/variables.tf
+++ dns_records/variables.tf
@@ -2,4 +2,6 @@ variable "region" {
     default = "us-east-1"
 }
 
-variable "master_account_number" {}
+variable "master_account_numbers" {
+    type = set(string)
+}
